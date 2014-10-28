1bfcc3687e82c087ea50d816179350be5d716d06
Tanjo, Hiroyuki <expheno@gmail.com>
2014-10-28 20:41:29 +0900

    Output fixed and fluid layout

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,32 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var merge = require('event-stream').merge;
 var del = require('del');
 var cleanup = require('./tasks/cleanup');
 
 gulp.task('build', function (cb) {
-  return gulp.src('bower_components/github-markdown-css/github-markdown.css')
-    .pipe(cleanup())
-    .pipe($.minifyCss())
-    .pipe($.rename('githubish.min.css'))
-    .pipe(gulp.dest('dist'))
-    .pipe($.cssbeautify())
-    .pipe($.csscomb())
-    .pipe($.rename('githubish.css'))
-    .pipe(gulp.dest('dist'));
+  return merge(
+    gulp.src(['bower_components/github-markdown-css/github-markdown.css', 'src/fixed.css'])
+      .pipe($.concat('githubish.min.css'))
+      .pipe(cleanup())
+      .pipe($.minifyCss())
+      .pipe(gulp.dest('dist'))
+      .pipe($.cssbeautify())
+      .pipe($.csscomb())
+      .pipe($.rename('githubish.css'))
+      .pipe(gulp.dest('dist')),
+
+    gulp.src(['bower_components/github-markdown-css/github-markdown.css', 'src/fluid.css'])
+      .pipe($.concat('githubish-fluid.min.css'))
+      .pipe(cleanup())
+      .pipe($.minifyCss())
+      .pipe(gulp.dest('dist'))
+      .pipe($.cssbeautify())
+      .pipe($.csscomb())
+      .pipe($.rename('githubish-fluid.css'))
+      .pipe(gulp.dest('dist'))
+  );
 });
 
 gulp.task('clean', function (cb) {
