2d780e3c2aac98f660996a13e08554e9aa3f3ab2
Tanjo, Hiroyuki <expheno@gmail.com>
2014-10-28 18:32:58 +0900

    Depends on github-markdown-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,19 @@
 'use strict';
 
-var fs = require('fs');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-var got = require('got');
-var cheerio = require('cheerio');
-var uncss = require('uncss');
+var del = require('del');
 var cleanup = require('./tasks/cleanup');
 
-var FIXTURE = 'https://github.com/htanjo/github-md.css/blob/master/src/fixture.md';
-
-function getRenderedFixture(cb) {
-  got(FIXTURE, function (err, data) {
-    if (err) {
-      cb(err);
-      return;
-    }
-    var $ = cheerio.load(data);
-    var body = $('.markdown-body');
-    var html = $.html($('body').html(body).closest('html'));
-    cb(null, html);
-  });
-}
-
 gulp.task('build', function (cb) {
-
-  getRenderedFixture(function (err, html) {
-    if (err) {
-      cb(err);
-      return;
-    }
-    uncss(html, {}, function (err, css) {
-      if (err) {
-        cb(err);
-        return;
-      }
-      fs.writeFileSync('src/github-md.css', css);
-
-      gulp.src('src/*.css')
-        .pipe(cleanup())
-        .pipe(gulp.dest('dist'))
-        .on('end', function () {
-          cb();
-        });
-
-    });
-  });
-
+  return gulp.src('bower_components/github-markdown-css/github-markdown.css')
+    .pipe(cleanup())
+    .pipe($.rename('githubish.css'))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['build']);
+gulp.task('clean', function (cb) {
+  del(['dist'], cb);
+});
+
+gulp.task('default', ['clean', 'build']);
