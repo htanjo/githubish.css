50709e04ee26342f797ba92c21e1f77066bbfd99
Tanjo, Hiroyuki <expheno@gmail.com>
2014-11-18 13:21:22 +0900

    Add "gulp pages" task to generate demo pages

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var merge = require('event-stream').merge;
 var del = require('del');
 var cleanup = require('./tasks/cleanup');
 
-gulp.task('build', function (cb) {
+gulp.task('build', function () {
   return merge(
     gulp.src(['bower_components/github-markdown-css/github-markdown.css', 'src/fixed.css'])
       .pipe($.concat('githubish.min.css'))
@@ -34,4 +34,28 @@ gulp.task('clean', function (cb) {
   del(['dist'], cb);
 });
 
+gulp.task('build-pages', function () {
+  return merge(
+    gulp.src('README.md')
+      .pipe($.markdown())
+      .pipe($.wrap({src: 'templates/fixed.html'}))
+      .pipe($.rename('index.html'))
+      .pipe(gulp.dest('gh-pages')),
+
+    gulp.src('README.md')
+      .pipe($.markdown())
+      .pipe($.wrap({src: 'templates/fluid.html'}))
+      .pipe($.rename('fluid.html'))
+      .pipe(gulp.dest('gh-pages')),
+
+    gulp.src(['dist/githubish.css', 'dist/githubish-fluid.css'])
+      .pipe(gulp.dest('gh-pages'))
+  );
+});
+
+gulp.task('clean-pages', function (cb) {
+  del(['gh-pages'], cb);
+});
+
 gulp.task('default', ['clean', 'build']);
+gulp.task('pages', ['clean-pages', 'build-pages']);
